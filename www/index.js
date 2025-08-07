8e4cfbe1d8e299a34f41cef68813737aeead6aed
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-08-07 08:09:04 -0400

    chore: Added packaged Wasm code to the imported dependencies of the web project

--- www/index.js
+++ www/index.js
@@ -1,28 +1,5 @@
-async function init() {
-  // accessing JS memory from Wasm
-  const memory = new WebAssembly.Memory({ initial: 1 });
-  const importObject = {
-    js: {
-      mem: memory,
-    },
-    console: {
-      log: () => {
-        console.log("Just logging something!");
-      },
-      error: () => {
-        console.error("I am just error");
-      },
-    },
-  };
+import init, { greet } from "wasm_snake_game_udemy";
 
-  // browser native function
-  const response = await fetch("sum.wasm");
-  const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer, importObject);
-
-  const uint8Array = new Uint8Array(memory.buffer, 0, 2); // only get the first two bytes - the word "Hi"
-  const hiText = new TextDecoder().decode(uint8Array);
-  console.log(hiText);
-}
-
-init();
+init().then((_) => {
+  greet("Vasile");
+});
