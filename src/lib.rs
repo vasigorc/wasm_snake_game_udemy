fca0a36b5bf1f2889acaab2f428d9986a90a8da0
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-08-08 05:30:03 -0400

    chore: Import JS function into Wasm

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,11 @@ use wasm_bindgen::prelude::*;
 // we need to mark `pub` functions that we want to export to JavaScript
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    println!("Hi there {name}");
+    alert(name);
 }
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
