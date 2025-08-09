a60f81dccd36810afafc20c0811493e277d08609
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-08-09 08:04:20 -0400

    chore: Switched to wee_alloc
    wee_alloc is a memory allocator specifically designed for WebAssembly
    (Wasm) environments, prioritizing small code size over raw performance.
    Its primary goal is to minimize the compiled .wasm binary size, which is
    crucial for applications transmitted over the network and requiring fast
    loading times.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 // we need to mark `pub` functions that we want to export to JavaScript
 #[wasm_bindgen]
