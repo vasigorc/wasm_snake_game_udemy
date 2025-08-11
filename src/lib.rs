9ea92761134db28ff41590a00a4139362921c234
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2025-08-11 07:57:57 -0400

    feat: Added snake struct

--- src/lib.rs
+++ src/lib.rs
@@ -8,21 +8,43 @@ static ALLOC: WeeAlloc = WeeAlloc::INIT;
 #[wasm_bindgen]
 pub struct World {
     width: usize,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new() -> World {
-        World { width: 16 }
+        World {
+            width: 16,
+            snake: Snake::new(10),
+        }
     }
 
     pub fn width(&self) -> usize {
         self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
     }
 }
 
 impl Default for World {
     fn default() -> Self {
         World::new()
+    }
+}
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec![SnakeCell(spawn_index)],
+        }
     }
 }
